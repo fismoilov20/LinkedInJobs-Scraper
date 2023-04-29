64aaa3bb5987fc0e1be3c2574be05ababd2123a5
fismoilov20 <fismoilov20@gmail.com>
2023-04-29 16:22:25 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# LinkedIn Scraper
+
+# Getting Started
+1. Clone this project: `git clone https://github.com/python-scrapy-playbook/basic-scrapy-project.git`
+2. Create a Python Virtual Environment: `python3 -m venv venv`
+3. Activate the Python Virtual Environment: `source venv/bin/activate`
+4. Install Scrapy using pip: `pip install scrapy`
+5. Install Proxy using pip: `pip install scrapeops-scrapy-proxy-sdk`
+    Go to `settings.py` add your API key and the following lines:
+    ```
+    ROBOTSTXT_OBEY = False
+
+    SCRAPEOPS_API_KEY = 'YOUR_API_KEY'
+
+    SCRAPEOPS_PROXY_ENABLED = True
+
+    DOWNLOADER_MIDDLEWARES = {
+        'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk. ScrapeOpsScrapyProxySdk': 725,
+    }
+    ```
+ To use the ScrapeOps Proxy you need to first install the monitoring SDK: `pip install scrapeops-scrapy`
+ Go to `settings.py`and add the following lines:
+ ```
+    EXTENSIONS = {
+    'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+}
+    DOWNLOADER_MIDDLEWARES = {
+    ## ScrapeOps Monitor
+    'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+    
+    ## Proxy Middleware
+    'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk': 725,
+}
+ ```
+ Listing the scrapy projects `scrapy list`
+ 
+
+ # To Run the Project
+ Running the scrapy project: `scrapy crawl linkedinjobs`
+ The extracted job listings will be stored in a JSON file called linkedin_jobs.json
---
+++ linkedin-jobs-scraper/spiders/linkedinjobs.py
@@ -0,0 +1,42 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
+from scrapeops_scrapy_proxy_sdk.middlewares import ScrapeOpsProxyMiddleware
+from scrapeops_scrapy.middlewares import ScrapeOpsMiddleware
+
+class LinkedInJobItem(scrapy.Item):
+    Application_deadline = scrapy.Field()
+    Location = scrapy.Field()
+    Employment_form = scrapy.Field()
+    Employment_type = scrapy.Field()
+    Salary_Range = scrapy.Field()
+    Education_level = scrapy.Field()
+    Years_of_Experience = scrapy.Field()
+    Required_languages = scrapy.Field()
+    Markets = scrapy.Field()
+    Skills = scrapy.Field()
+    Description = scrapy.Field()
+
+class LinkedInSpider(scrapy.Spider):
+    name = 'linkedin_spider'
+    start_urls = ['https://www.linkedin.com/jobs/search?keywords=Software%20Engineer&location=United%20States']
+
+    custom_settings = {
+        'DOWNLOADER_MIDDLEWARES': {
+            ScrapeOpsProxyMiddleware: 400,
+            ScrapeOpsMiddleware: 800
+        },
+        'SCRAPEOPS_EMAIL': 'your@email.com',
+        'SCRAPEOPS_API_KEY': 'your_scrapeops_api_key',
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'linkedin_jobs.json'
+    }
+
+    def parse(self, response):
+        job_listings = response.xpath('//div[contains(@class, "result-card__contents")]')
+        for job in job_listings:
+            loader = ItemLoader(item=LinkedInJobItem(), selector=job)
+            # Extract and map fields from the job listing's HTML
+            # ...
+
+            yield loader.load_item()
